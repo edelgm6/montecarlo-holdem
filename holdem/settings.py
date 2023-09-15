dfe31d448e6f3411146a064f942a409c96c94ddc
Garrett Edel <edelgm6@gmail.com>
2023-09-15 11:15:29 -0400

    Updated static files root

--- holdem/settings.py
+++ holdem/settings.py
@@ -129,8 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR + "staticfiles"
+STATIC_ROOT = BASE_DIR + "/staticfiles"
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 try:
     from holdem.local_settings import *
