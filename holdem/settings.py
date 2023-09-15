922be332a32035324f4adac4cb3e791779734303
Garrett Edel <edelgm6@gmail.com>
2023-09-15 11:19:50 -0400

    rolled back

--- holdem/settings.py
+++ holdem/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-# STATIC_ROOT = BASE_DIR + "/staticfiles"
+STATIC_ROOT = BASE_DIR + "/staticfiles"
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
