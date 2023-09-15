7364c67c1e2490d639b910e1b62c0d0b4a9131de
Garrett Edel <edelgm6@gmail.com>
2023-09-15 11:06:06 -0400

    added static_root

--- holdem/settings.py
+++ holdem/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = '/static/'
 
 try:
